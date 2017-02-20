72a7dbb3b5fbd006efb949549d8a46ac28555f58
adminemc <sonyrenzmanalo@live.com>
2017-02-20 13:26:15 +0800

    config
    for testing

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+global $settings;
+/*
+$settings['db_name'] = "pipeline";
+$settings['db_server'] = "10.20.39.7:3306";
+$settings['db_user'] = "root";
+$settings['db_password'] = "Incognit0"; 
+*/
+$settings['db_name'] = "pipeline";
+$settings['db_server'] = "10.20.39.122";
+$settings['db_user'] = "root"; 
+$settings['db_password'] = "Welcome1"; 
+
+//$settings['web_host'] = "http://localhost/schedule/";
+//$settings['path'] = "{$_SERVER['DOCUMENT_ROOT']}/schedule/";
+
+?>
